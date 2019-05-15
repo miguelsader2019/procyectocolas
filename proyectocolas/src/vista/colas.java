14bca0bfe86cf88f38f4cff143a275132015a4b2
miguel angel <rander2020.120.drz32@gmail.com>
2019-05-15 13:33:20 -0500

    adios amor

---
+++ proyectocolas/src/vista/colas.java
@@ -0,0 +1,20 @@
+
+package vista;
+
+/**
+ *
+ * @author miguel angel
+ */
+public class colas {
+    
+    public static void main(String[]arg){
+        
+      System.out.print("hola");  
+        
+        
+    }
+    
+    
+    
+    
+}
